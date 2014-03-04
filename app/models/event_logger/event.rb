059500c0301ca73b70a0b21da41643c3ab1b61b7
Vadim Lobanov <vadim@lobanov.pw>
2014-03-04 13:39:55 +0700

    basic fields

--- app/models/event_logger/event.rb
+++ app/models/event_logger/event.rb
@@ -1,5 +1,10 @@
 module EventLogger
   class Event
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :event_type, type: String
+    field :event_subtype, type: String
     
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,26 @@
 require "spec_helper"
 
 describe EventLogger::Event do
+  it "is mongoid model" do
+    EventLogger::Event.ancestors.should include(Mongoid::Document)
+  end
 
+  it "has timestamps" do
+    EventLogger::Event.ancestors.should include(Mongoid::Timestamps)
+  end
+
+  it "has event_type field" do
+    e = EventLogger::Event.create(event_type: "train_coming")
+    EventLogger::Event.last.event_type.should == "train_coming"
+  end
+
+  it "has event_subtype field" do
+    e = EventLogger::Event.create(event_subtype: "train_with_supplies")
+    EventLogger::Event.last.event_subtype.should == "train_with_supplies"
+  end
+
+  it "has created_at field" do
+    e = EventLogger::Event.create(event_type: "train_coming", event_subtype: "train_with_supplies")
+    (Time.now - EventLogger::Event.last.created_at).should be < 2.seconds
+  end
 end
