fa151c8ef5bf91f1b1e077e96b36d97f8137ba4f
Vadim Lobanov <vadim@lobanov.pw>
2014-03-04 20:05:31 +0700

    rename Exception to CaughtException. more symbols, less mess

--- app/models/event_logger/event.rb
+++ app/models/event_logger/event.rb
@@ -7,6 +7,6 @@ module EventLogger
     field :event_subtype, type: Symbol
     field :warning_level, type: String
 
-    embeds_one :exception
+    embeds_one :caught_exception
   end
 end
--- app/models/event_logger/exception.rb
+++
@@ -1,4 +0,0 @@
-class EventLogger::Exception
-  include Mongoid::Document
-  embedded_in :event
-end
