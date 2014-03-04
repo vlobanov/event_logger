ba3550451427b55e36cb06eeb52e12c00a5b00dd
Vadim Lobanov <vadim@lobanov.pw>
2014-03-04 20:51:53 +0700

    formatting

--- app/models/event_logger/event.rb
+++ app/models/event_logger/event.rb
@@ -6,7 +6,13 @@ module EventLogger
     field :event_type, type: Symbol
     field :event_subtype, type: Symbol
     field :warning_level, type: String
+    field :description, type: String
 
     embeds_one :caught_exception
+
+    def to_s
+      ["#{event_type}:#{event_subtype}",
+        description].compact.join(" ")
+    end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -19,6 +19,11 @@ describe EventLogger::Event do
     EventLogger::Event.last.event_subtype.should == :train_with_supplies
   end
 
+  it "has description field" do
+    e = EventLogger::Event.create(description: "Wow!")
+    EventLogger::Event.last.description.should == "Wow!"
+  end
+
   it "has created_at field" do
     e = EventLogger::Event.create(event_type: :train_coming, event_subtype: :train_with_supplies)
     (Time.now - EventLogger::Event.last.created_at).should be < 2.seconds
@@ -29,5 +34,29 @@ describe EventLogger::Event do
     exc = StandardError.new("a message")
     e.caught_exception = EventLogger::CaughtException.new({})
     e.save!
+  end
+
+  describe "#to_s formatting by default" do
+    let(:event) { EventLogger::Event.create(event_type: :train_coming, event_subtype: :we_are_waiting, description: "well well") }
+
+    it "is not ugly" do
+      event.to_s.should_not include("#<EventLogger::Event")
+    end
+
+    it "includes event type" do
+      event.to_s.should include event.event_type.to_s
+    end
+
+    it "includes event subtype" do
+      event.to_s.should include event.event_subtype.to_s
+    end
+
+    it "includes event description" do
+      event.to_s.should include event.description.to_s
+    end
+
+    it "works if only event_type is set" do
+      EventLogger::Event.create(event_type: :train_coming).to_s.should include "train_coming"
+    end
   end
 end
