7cd49289af129c01bf2655738469be9306087a4f
Vadim Lobanov <vadim@lobanov.pw>
2014-03-05 15:04:04 +0700

    exception handling

--- lib/event_logger/logger.rb
+++ lib/event_logger/logger.rb
@@ -1,8 +1,23 @@
 module EventLogger
   class Logger
-    def log(event_type, description = nil)
-      yield if block_given?
-      Event.create(event_type: event_type, description: description)
+    def log(event_type, *args)
+      caught_exception = nil
+      begin
+        yield if block_given?
+      rescue Exception => e
+        caught_exception = e
+        raise e
+      ensure
+        create_event(event_type, caught_exception, *args)
+      end
+    end
+
+    private
+    
+    def create_event(event_type, exception, description = nil)
+      event = Event.new(event_type: event_type, description: description)
+      event.caught_exception = CaughtException.new(exception) if exception
+      event.save!
     end
   end
 end
--- spec/models/logger_spec.rb
+++ spec/models/logger_spec.rb
@@ -16,6 +16,11 @@ describe EventLogger::Logger do
       EventLogger::Event.last.event_type.should == :something_weird
     end
 
+    it "doesn't have caught_exception if nothing was raised" do
+      subject.log(:event, "meatball")
+      EventLogger::Event.last.caught_exception.should be_nil
+    end
+
     context "when a block is given" do
       it "is executed" do
         expect { |probe| subject.log(:event, &probe) }.to yield_control
@@ -26,10 +31,49 @@ describe EventLogger::Logger do
         subject.log(:event) { events_count = EventLogger::Event.count }
         (EventLogger::Event.count - events_count).should be == 1
       end
-    end
 
-    describe "exception handling" do
+      context "when an exception is raised inside the block" do
+        let(:example_exception) { StandardError.new("Boom") }
 
+        it "is re-raised" do
+          expect do
+            subject.log(:exploded_event) { raise example_exception }
+          end.to raise_exception(example_exception.class, example_exception.message)
+        end
+
+        describe "event" do
+          it "is still logged" do
+            expect do
+              subject.log(:exploded_event) { raise example_exception }
+            end.to raise_exception(example_exception.class)
+
+            EventLogger::Event.last.event_type.should == :exploded_event
+          end
+
+          describe "caught exception" do
+            it "is persisted" do
+              subject.log(:exploded_event) { raise example_exception } rescue :dont_care
+              EventLogger::Event.last.caught_exception.should_not be_nil
+            end
+
+            it "has correct message field value" do
+              subject.log(:exploded_event) { raise example_exception } rescue :dont_care
+              EventLogger::Event.last.caught_exception.message.should == example_exception.message
+            end
+
+            it "has correct class_name field value" do
+              AmazingException = Class.new(StandardError)
+              subject.log(:exploded_event) { raise AmazingException } rescue :dont_care
+              EventLogger::Event.last.caught_exception.class_name.should == "AmazingException"
+            end
+
+            it "has correct backtrace field value" do
+              subject.log(:exploded_event) { raise example_exception } rescue :dont_care
+              EventLogger::Event.last.caught_exception.backtrace.detect{|f| f[__FILE__]}.should_not be_nil
+            end
+          end
+        end
+      end
     end
   end
 end
