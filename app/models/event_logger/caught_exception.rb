ec82ff37543f41de46b423a0b23f6ab7952fe1a3
Vadim Lobanov <vadim@lobanov.pw>
2014-03-06 16:08:54 +0700

    module nesting

--- app/models/event_logger/caught_exception.rb
+++ app/models/event_logger/caught_exception.rb
@@ -1,26 +1,28 @@
-class EventLogger::CaughtException
-  include Mongoid::Document
-  embedded_in :event, class_name: "EventLogger::Event"
+module EventLogger
+  class CaughtException
+    include Mongoid::Document
+    embedded_in :event, class_name: "EventLogger::Event"
 
-  field :class_name, type: String
-  field :message, type: String
-  field :backtrace, type: Array
+    field :class_name, type: String
+    field :message, type: String
+    field :backtrace, type: Array
 
-  def initialize(attrs_or_exception = nil)
-    if attrs_or_exception.kind_of?(Exception)
-      source_exception = attrs_or_exception.dup
-      attrs = {
-        class_name: source_exception.class.name,
-        message: source_exception.message,
-        backtrace: source_exception.backtrace
-      }
-      super(attrs)
-    else
-      super
+    def initialize(attrs_or_exception = nil)
+      if attrs_or_exception.kind_of?(Exception)
+        source_exception = attrs_or_exception.dup
+        attrs = {
+          class_name: source_exception.class.name,
+          message: source_exception.message,
+          backtrace: source_exception.backtrace
+        }
+        super(attrs)
+      else
+        super
+      end
     end
-  end
 
-  def to_s
-    [class_name, message].join(" ")
+    def to_s
+      [class_name, message].join(" ")
+    end
   end
 end
