2a255d4c1a2d9648c31e08cf3369d0ad9b006ba2
Vadim Lobanov <vadim@lobanov.pw>
2014-03-06 23:06:43 +0700

    rename layout and set it explicitly

--- app/controllers/event_logger/events_controller.rb
+++ app/controllers/event_logger/events_controller.rb
@@ -1,5 +1,7 @@
 module EventLogger
   class EventsController < ApplicationController
+    layout 'event_logger/even_logger_application'
+
     def index
       @events = Event.order_by(created_at: "desc")
     end
