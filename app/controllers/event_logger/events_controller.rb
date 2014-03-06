019d08ffb64c8aa65f4db0bda75d56910fdc042f
Vadim Lobanov <vadim@lobanov.pw>
2014-03-06 14:35:21 +0700

    rm test event

--- app/controllers/event_logger/events_controller.rb
+++ app/controllers/event_logger/events_controller.rb
@@ -2,7 +2,6 @@ module EventLogger
   class EventsController < ApplicationController
     def index
       @events = Event.order_by(created_at: "desc")
-      EventLogger::Logger.new.log(:index_page_opened, "hello, world!")
     end
   end
 end
