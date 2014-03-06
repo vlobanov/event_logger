8411ab6410e79132ce92c80d1b1d36c204be8acf
Vadim Lobanov <vadim@lobanov.pw>
2014-03-06 14:25:54 +0700

    minimal presentation

--- app/controllers/event_logger/events_controller.rb
+++ app/controllers/event_logger/events_controller.rb
@@ -1,7 +1,8 @@
 module EventLogger
   class EventsController < ApplicationController
     def index
-      render :index
+      @events = Event.order_by(created_at: "desc")
+      EventLogger::Logger.new.log(:index_page_opened, "hello, world!")
     end
   end
 end
