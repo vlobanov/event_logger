1ce119ad86ea4d46c2008629a8fc6bb8eba94f67
Vadim Lobanov <vadim@lobanov.pw>
2014-03-06 22:46:25 +0700

    browsing specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
   factory :event, class: EventLogger::Event do
-    event_type :simple_event
+    event_type :nice_event
     description "ordinary thing"
 
     factory :event_with_exception do
       caught_exception EventLogger::CaughtException.new(StandardError.new("a bang"))
+
+      factory :event_full do
+        messages ["first msg", "second msg"]
+      end
     end
   end
 end
--- spec/features/events_browsing_spec.rb
+++ spec/features/events_browsing_spec.rb
@@ -11,8 +11,25 @@ feature "Events browsing" do
   scenario "User sees simple events" do
     create(:event, description: "first desc")
     create(:event, description: "second desc")
+
     visit event_logger_path
+
     page.should have_content("first desc")
     page.should have_content("second desc")
+  end
+
+  scenario "User sees event exceptions" do
+    create(:event_with_exception)
+
+    visit event_logger_path
+
+    page.should have_content("StandardError")
+    page.should have_content("a bang")
+  end
+
+  scenario "User sees event messages" do
+    e = create(:event_full)
+    visit event_logger_path
+    e.messages.each { |msg| page.should have_content(msg) }
   end
 end
