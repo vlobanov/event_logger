c22306270c6614520e973fa7473beb8376592c3d
Vadim Lobanov <vadim@lobanov.pw>
2014-03-07 15:45:47 +0700

    filtering by exceptions

--- app/controllers/event_logger/events_controller.rb
+++ app/controllers/event_logger/events_controller.rb
@@ -4,10 +4,18 @@ module EventLogger
 
     def index
       @events = Event.order_by(created_at: "desc")
-      if params[:filter_exceptions]
-        @events = @events.where(:caught_exception.ne => nil)
-      end
+      @events = filter_exceptions(@events)
       @events = @events.page(params[:page]).per(50)
+    end
+
+    private
+
+    def filter_exceptions(events)
+      case params[:filter_exceptions]
+        when "with_exceptions" then events.where(:caught_exception.ne => nil)
+        when "without_exceptions" then events.where(:caught_exception => nil)
+        else events
+      end
     end
   end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -18,10 +18,14 @@ describe EventLogger::EventsController do
   describe "filtering" do
     it "filters events with exceptions" do
       events = events_in_all_combinations
-
       get :index, filter_exceptions: :with_exceptions
-
       expect(assigns(:events).to_a).to eq(events[:with_exceptions])
+    end
+    
+    it "filters events without exceptions" do
+      events = events_in_all_combinations
+      get :index, filter_exceptions: :without_exceptions
+      expect(assigns(:events).to_a).to eq(events[:without_exceptions])
     end
   end
 
@@ -33,7 +37,7 @@ describe EventLogger::EventsController do
         create(:event_full, created_at: decreasing_created_at),
         create(:event_full, created_at: decreasing_created_at)
       ],
-      normal: [
+      without_exceptions: [
         create(:event, created_at: decreasing_created_at),
         create(:event, created_at: decreasing_created_at)
       ]
