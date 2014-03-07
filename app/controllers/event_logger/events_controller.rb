7837889381d1a7136d70c733c89c2bf5b857d244
Vadim Lobanov <vadim@lobanov.pw>
2014-03-07 12:49:20 +0700

    typo

--- app/controllers/event_logger/events_controller.rb
+++ app/controllers/event_logger/events_controller.rb
@@ -1,6 +1,6 @@
 module EventLogger
   class EventsController < ApplicationController
-    layout 'event_logger/even_logger_application'
+    layout 'event_logger/event_logger_application'
 
     def index
       @events = Event.order_by(created_at: "desc")
