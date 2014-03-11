8d39725d564fb99a6a00c3cc5882ab15bb908711
Vadim Lobanov <vadim@lobanov.pw>
2014-03-11 12:59:43 +0700

    convert to full engine

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-EventLogger::Engine.routes.draw do
-  match "/filter(/:page)" => "events#index", via: :get, as: :events_filter
-  match "/(:page)" => "events#index", via: :get
-  root to: "events#index", page: 1
-end
--- lib/event_logger/engine.rb
+++ lib/event_logger/engine.rb
@@ -1,9 +1,5 @@
-require 'event_logger/protect_filter'
-
 module EventLogger
   class Engine < ::Rails::Engine
-    isolate_namespace EventLogger
-
     initializer  "event_logger.define_protect_methods" do
       ActiveSupport.on_load(:action_controller) do
         extend EventLogger::ProtectFilter
