715f9cfddc1ea5196342285445a6b0e6291025be
Vadim Lobanov <vadim@lobanov.pw>
2014-03-11 13:20:23 +0700

    move protection specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,8 +1,6 @@
 require "spec_helper"
 
 describe EventLogger::EventsController do
-  before(:each) { @routes = EventLogger::Engine.routes }
-
   it "opens successfully" do
     get :index
   end
@@ -85,6 +83,34 @@ describe EventLogger::EventsController do
         get :index, filter_time_to: to_time
       
         assigns(:events).to_a.should =~ [week_old]
+      end
+    end
+
+    describe "::protect_event_logger" do
+      specify do
+        visit event_logger_path
+        ApplicationController.should respond_to :protect_event_logger 
+      end
+
+      it "adds before filters as blocks" do
+        hints = 5
+        expect do
+          hints.times{ visit event_logger_path }
+        end.to change{ $before_filter_block_counter || 0 }.by(hints)
+      end
+
+      it "adds before filters as methods" do
+        hints = 5
+        expect do
+          hints.times{ visit event_logger_path }
+        end.to change{ $before_filter_method_counter || 0 }.by(hints)
+      end
+
+      it "does not add before filters to application controllers" do
+        hints = 5
+        expect do
+          hints.times{ visit posts_path }
+        end.not_to change{ $before_filter_method_counter || 0 }
       end
     end
   end
