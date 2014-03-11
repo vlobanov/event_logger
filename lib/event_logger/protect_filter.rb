a1ab7bba896096271d3f73ca2d279242c9a2bf81
Vadim Lobanov <vadim@lobanov.pw>
2014-03-11 13:20:53 +0700

    avoid circular dependency with if condition

--- lib/event_logger/protect_filter.rb
+++ lib/event_logger/protect_filter.rb
@@ -1,12 +1,12 @@
 module EventLogger
   module ProtectFilter
     def protect_event_logger(method_name = nil, &block)
-      EventLogger::EventsController.class_eval do
-        if method_name
-          before_filter(method_name)
-        elsif block
-          before_filter(&block)
-        end
+      is_event_logger_controller = proc { |c| c.class == EventLogger::EventsController }
+      
+      if method_name
+        before_filter method_name, if: is_event_logger_controller
+      elsif block
+        before_filter if: is_event_logger_controller, &block
       end
     end
   end
